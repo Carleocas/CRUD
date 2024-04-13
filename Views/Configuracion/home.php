72bdff1ab9b730adf313e0035d28a91a327b0113
Carleocas <castro-carlos1626@unesum.edu.ec>
2024-04-13 16:59:41 -0500

    Initial commit

---
+++ Views/Configuracion/home.php
@@ -0,0 +1,12 @@
+<?php include "Views/Templates/header.php"; ?>
+ 
+    <div class="col-md-6 col-lg-3">
+        <div class="widget-small info coloured-icon"><i class="icon fa fa-book fa-3x"></i>
+            <a class="info" href="<?php echo base_url; ?>Libros">
+                <h4>Libros</h4>
+                <p><b><?php echo $data['libros']['total'] ?></b></p>
+            </a>
+        </div>
+    </div>
+
+<?php include "Views/Templates/footer.php"; ?>
